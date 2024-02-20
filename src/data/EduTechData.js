e0423cd1c451dd97476b7f187aa9c53c3f688db4
VRPIGC <vrpigroup@gmail.com>
2024-02-20 10:23:09 +0530

    Cloned the project

---
+++ src/data/EduTechData.js
@@ -0,0 +1,150 @@
+const mainContent = {
+  head: "FOR HUNDREDS OF SUCCESSFUL STUDENTS HERE EMBRACE OUR NEW COMMUNITY EXPLORE AND GROW",
+  description: "If you are DETERMINED to learn, no one can stop YOU!!",
+};
+
+const WhatIsSection = {
+  title: "What is Edu-Tech Program ?",
+  highlightWord: "Edu-Tech",
+  image: "Edutech1.png",
+  description:
+    "EduTech integrates education and technology, employing digital tools such as online platforms, educational apps, and virtual reality to enrich learning. Its objectives include personalized education, enhanced teaching efficacy, and broader access to quality learning. EduTech's ultimate goal is to equip learners with vital skills for thriving in the digital era.",
+};
+
+const cards = {
+  courses: true,
+  internships: false,
+  multipleCardsSection: {
+    paidCourse: {
+      title: "Edu-Tech Free Courses",
+      highlightWord: "Edu-Tech",
+      courses: [
+        {
+          name: "Java Full-Stack",
+          courseLink: "/",
+          content: "Free Certificate Course and Virtual Classes",
+          price: "₹1799",
+          buttonContent: "Enroll Now",
+        },
+        {
+          name: "DevOps with Cloud",
+          courseLink: "/",
+          content: "Free Certificate Course and Virtual Classes",
+          price: "₹1799",
+          buttonContent: "Enroll Now",
+        },
+      ],
+    },
+
+    freeCourse: {
+      type: "freeCourse",
+      title: "Edu-Tech Paid Courses",
+      highlightWord: "Edu-Tech",
+
+      courses: [
+        {
+          name: "Embedded & IoT",
+          courseLink: "/",
+          content:
+            "Certificate Course, Virtual Classes and Live Practical Training",
+          buttonContent: "Enroll Now",
+        },
+        {
+          name: "Artificial Intelligence",
+          courseLink: "/",
+          content:
+            "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+          // price: "₹1799",
+          buttonContent: "Enroll Now",
+        },
+      ],
+    },
+  },
+};
+
+const benefitsData = {
+  title: "Edu-Tech Benefits",
+  highlightWord: "Edu-Tech",
+  head: "EduTech has the potential to transform education, making it more accessible, engaging, and effective for learners of all ages and backgrounds.",
+  benefits: ["Certification", "Recorded Videos", "Classes for free"],
+  image: "Edutech3.png",
+};
+
+const InstructorSection = {
+  title: "Edu-Tech Instructors",
+  highlightWord: "Edu-Tech",
+  Instructors: [
+    {
+      name: "Neil",
+      address: "Instructor1.png",
+      technologies: "Python Full Stack",
+    },
+    {
+      name: "Aryan",
+      address: "Instructor2.png",
+      technologies: "Java Full Stack",
+    },
+    { name: "Chritee", address: "Instructor3.png", technologies: "Devops" },
+    {
+      name: "Lucy",
+      address: "Instructor4.png",
+      technologies: "Embedded & Iot",
+    },
+    {
+      name: "Aryan",
+      address: "Instructor2.png",
+      technologies: "Java Full Stack",
+    },
+    { name: "Chritee", address: "Instructor3.png", technologies: "Devops" },
+    {
+      name: "Lucy",
+      address: "Instructor4.png",
+      technologies: "Embedded & Iot",
+    },
+    {
+      name: "Aryan",
+      address: "Instructor2.png",
+      technologies: "Java Full Stack",
+    },
+    { name: "Chritee", address: "Instructor3.png", technologies: "Devops" },
+    {
+      name: "Lucy",
+      address: "Instructor4.png",
+      technologies: "Embedded & Iot",
+    },
+  ],
+};
+
+const FAQs = [
+  {
+    question: "How many courses can be taken for enrollment?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+  {
+    question:
+      "How long does it typically take for a student to complete the course? ",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+  {
+    question:
+      "For what kinds of jobs and positions does the program train its students?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+  {
+    question: "How old do i have to be to take on online class?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+];
+
+export const EduTechData = {
+  mainContent,
+  WhatIsSection,
+  cards,
+  benefitsData,
+  InstructorSection,
+  FAQs,
+};
---
+++ src/data/InternshipsData.js
@@ -0,0 +1,146 @@
+const mainContent = {
+  head: '"The best way to predict the future is to create it."',
+  description:
+    '"Internships offer a unique opportunity to step out of your comfort zone, challenge yourself, and discover what you are truly capable of achieving."',
+};
+
+const WhatIsSection = {
+  title: "Why an Internship?",
+  highlightWord: "Internships",
+  image: "Internship1.png",
+  description:
+    "Internships offer hands-on experience, linking classroom knowledge with practical skills. They allow for networking, skill development, and personal growth. Interns gain insights into their desired fields, enhancing their resumes and paving the way for future career success.",
+};
+
+const cards = {
+  courses: false,
+  internships: true,
+  title: "Internships on",
+  highlightWord: "Internships",
+  content: [
+    {
+      name: "Java Full-Stack",
+      courseLink: "#",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+      buttonContent: "Apply",
+    },
+    {
+      name: "Python Full-Stack",
+      courseLink: "#",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+      buttonContent: "Apply",
+    },
+    {
+      name: "DevOps with Cloud",
+      courseLink: "#",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+      // price: "$99.99",
+      buttonContent: "Apply",
+    },
+    {
+      name: "Data Engineering",
+      courseLink: "#",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+      // price: "$99.99",
+      buttonContent: "Apply",
+    },
+    {
+      name: "Artificial Intelligence",
+      courseLink: "/javascript-course",
+      content: "Master JavaScript programming language from scratch.",
+      buttonContent: "Apply",
+    },
+    {
+      name: "Embedded & IoT",
+      courseLink: "/javascript-course",
+      content: "Master JavaScript programming language from scratch.",
+      buttonContent: "Apply",
+    },
+  ],
+};
+
+const benefitsData = {
+  title: "Internships Benefits",
+  highlightWord: "Internships",
+  head: "  Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur.",
+  benefits: [
+    "Lorem ipsum dolor sit",
+    "Lorem ipsum dolor sit ",
+    "Lorem ipsum dolor sit ",
+    "Lorem ipsum dolor sit ",
+  ],
+  image: "Internship2.png",
+};
+
+// const InstructorSection = {
+//   title: "Edu-Tech Instructors",
+//   highlightWord: "Edu-Tech",
+//   Instructors: [
+//     {
+//       name: "Neil",
+//       address: "Instructor1.png",
+//       technologies: "Python Full Stack",
+//     },
+//     {
+//       name: "Aryan",
+//       address: "Instructor2.png",
+//       technologies: "Java Full Stack",
+//     },
+//     { name: "Chritee", address: "Instructor3.png", technologies: "Devops" },
+//     {
+//       name: "Lucy",
+//       address: "Instructor4.png",
+//       technologies: "Embedded & Iot",
+//     },
+//   ],
+// };
+
+const partnersWith = {
+  title: "Partners With",
+  partners: [
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+    { name: "" },
+  ],
+};
+
+const FAQs = [
+  {
+    question: "Lorem ipsum dolor sit amet consectetur ? ",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet consectetur ? ",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet consectetur ? ",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+  {
+    question: "Lorem ipsum dolor sit amet consectetur ? ",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur. ",
+  },
+];
+
+export const InternshipsData = {
+  mainContent,
+  WhatIsSection,
+  cards,
+  benefitsData,
+  partnersWith,
+  FAQs,
+};
