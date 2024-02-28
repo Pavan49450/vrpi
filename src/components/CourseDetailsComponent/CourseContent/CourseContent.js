ae7179e60bf2fe40ac91d8634a720503caec2691
Kattula Pavan Kumar <pavan49450@gmail.com>
2024-02-28 13:04:28 +0530

    Added Some styles to the Course Content Section.

--- src/components/CourseDetailsComponent/CourseContent/CourseContent.js
+++ src/components/CourseDetailsComponent/CourseContent/CourseContent.js
@@ -4,6 +4,7 @@ import Section from "../../../UI/Sections/Section";
 
 const CourseContent = ({ courseContent }) => {
   const [openChapters, setOpenChapters] = useState([]);
+  const [openModules, setOpenModule] = useState([]);
 
   const toggleChapter = (index) => {
     if (openChapters.includes(index)) {
@@ -13,8 +14,20 @@ const CourseContent = ({ courseContent }) => {
     }
   };
 
+  const toggleModule = (index) => {
+    if (openModules.includes(index)) {
+      setOpenModule(openModules.filter((item) => item !== index));
+    } else {
+      setOpenModule([...openModules, index]);
+    }
+  };
+
   const isChapterOpen = (index) => {
     return openChapters.includes(index);
+  };
+
+  const isModuleOpen = (index) => {
+    return openModules.includes(index);
   };
   return (
     <Section title="Course Content">
@@ -26,18 +39,67 @@ const CourseContent = ({ courseContent }) => {
               onClick={() => toggleChapter(chapterIndex)}
             >
               <h3>{chapter.title}</h3>
-              <span>{isChapterOpen(chapterIndex) ? "-" : "+"}</span>
+              {/* <span> */}
+              {isChapterOpen(chapterIndex) ? (
+                <img
+                  src={require("../../../assets/courses/arrowDown.png")}
+                  alt=""
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    transform: "rotate(180deg)",
+                  }}
+                ></img>
+              ) : (
+                <img
+                  src={require("../../../assets/courses/arrowDown.png")}
+                  alt=""
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                  }}
+                ></img>
+              )}
+              {/* </span> */}
             </div>
             {isChapterOpen(chapterIndex) && (
               <div className={styles.modules}>
                 {chapter.modules.map((module, moduleIndex) => (
-                  <div key={moduleIndex} className={styles.module}>
-                    <h4>{module.title}</h4>
-                    <ul>
-                      {module.lessons.map((lesson, lessonIndex) => (
-                        <li key={lessonIndex}>{lesson.title}</li>
-                      ))}
-                    </ul>
+                  <div
+                    key={moduleIndex}
+                    className={styles.module}
+                    onClick={() => toggleModule(moduleIndex)}
+                  >
+                    <div className={styles.moduleEach}>
+                      <h4>{module.title}</h4>
+                      {isModuleOpen(moduleIndex) ? (
+                        <img
+                          src={require("../../../assets/courses/arrowDown.png")}
+                          alt=""
+                          style={{
+                            width: "15px",
+                            height: "15px",
+                            transform: "rotate(180deg)",
+                          }}
+                        ></img>
+                      ) : (
+                        <img
+                          src={require("../../../assets/courses/arrowDown.png")}
+                          alt=""
+                          style={{
+                            width: "15px",
+                            height: "15px",
+                          }}
+                        ></img>
+                      )}
+                    </div>
+                    {isModuleOpen(moduleIndex) && (
+                      <ul>
+                        {module.lessons.map((lesson, lessonIndex) => (
+                          <li key={lessonIndex}>{lesson.title}</li>
+                        ))}
+                      </ul>
+                    )}
                   </div>
                 ))}
               </div>
