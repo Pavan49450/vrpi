ca7abc4e1013db24d9028579d794f6606aba2621
Kattula Pavan Kumar <pavan49450@gmail.com>
2024-03-06 18:03:28 +0530

    Gathered assets for dashboard header.

--- src/components/Dashboard/Layout/Header/DashboardHeader.js
+++ src/components/Dashboard/Layout/Header/DashboardHeader.js
@@ -1,14 +1,70 @@
+import { NavLink } from "react-router-dom";
+import Logo from "../../../Logo/Logo";
+import style from "./DashboardHeader.module.css";
+
 const DashboardHeader = () => {
   const DashboardLinks = [
-    { title: "My Dashboard", link: "/dashboard", icon: "" },
-    { title: "Edu-Tech Courses", link: "/dashboard/courses", icon: "" },
-    { title: "Internship", link: "/dashboard/internships", icon: "" },
-    { title: "Settings", link: "/dashboard/settings", icon: "" },
-    { title: "Logout", link: false, icon: "", action: () => {} },
-    { title: "Help & Support", link: "/dashboard/helpAndSupport", icon: "" },
+    {
+      title: "My Dashboard",
+      link: "/dashboard",
+      iconActive: "myDashboard-w.png",
+      iconInActive: "edutechIcon.png",
+    },
+    {
+      title: "Edu-Tech Courses",
+      link: "/dashboard/courses",
+      iconActive: "edutechIcon.png",
+      iconInActive: "edutechIcon.png",
+    },
+    {
+      title: "Internship",
+      link: "/dashboard/internships",
+      icon: "internshipIcon.png",
+    },
+    {
+      title: "Settings",
+      link: "/dashboard/settings",
+      icon: "settingsIcon.png",
+    },
+    { title: "Logout", link: false, icon: "logoutIcon.png", action: () => {} },
+    {
+      title: "Help & Support",
+      link: "/dashboard/helpAndSupport",
+      icon: "supportIcon.png",
+    },
   ];
 
-  return <div></div>;
+  return (
+    <header className={style.Header}>
+      <Logo />
+      <nav className={style.navbar}>
+        <ul className={style.nav}>
+          {DashboardLinks.map((link) => (
+            <li key={link.title}>
+              {link ? (
+                <NavLink to={link.link} className={style.navLink}>
+                  <img
+                    src={require(`../../../../assets/dashboard/${link.icon}`)}
+                    alt=""
+                  />
+                  <span>{link.title}</span>
+                </NavLink>
+              ) : (
+                <button onClick={link.action}>
+                  {" "}
+                  <img
+                    src={require(`../../../../assets/dashboard/${link.icon}`)}
+                    alt=""
+                  />
+                  <span>{link.title}</span>
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default DashboardHeader;
